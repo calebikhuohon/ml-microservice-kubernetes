59fb581cd14632cf38bff4afb6605fc2a24f899f
Caleb Ikhuohon <calebikhuohon@gmail.com>
2020-09-02 06:49:17 +0100

    update k8s deployment creation scripts

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=calebikhuohon/ml-microservice-k8s
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment ml-microservice-k8s --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployments/ml-microservice-k8s 8000:80
